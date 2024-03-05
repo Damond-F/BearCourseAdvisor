d5393123e64c2b6c893008b8a321606c4b2134e9
Danh <renevisk@gmail.com>
2024-03-04 19:33:40 -0800

    Added starter scraping

--- Webscraping/scrap.py
+++ Webscraping/scrap.py
@@ -3,4 +3,18 @@ from selenium.webdriver.common.keys import Keys
 import pandas as pd
 
 driver = webdriver.Chrome()
-driver.get('https://hoopshype.com/salaries/players/')
+driver.get('https://guide.berkeley.edu/courses/compsci/')
+
+
+
+code_element = driver.find_element("By.CSS_SELECTOR", "span.code")
+
+
+
+"""links = driver.find_elements("xpath", "//a[@href]") 
+for link in links:
+    print(link.get_attribute('innerHTML'))"""
+
+#driver.find_element("xpath", '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+
+
