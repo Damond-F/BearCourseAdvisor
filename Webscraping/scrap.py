b6b733c4989fe2a5167df212434c8c77954d44f5
Danh <renevisk@gmail.com>
2024-03-04 19:07:31 -0800

    Added starter scraping

--- Webscraping/scrap.py
+++ Webscraping/scrap.py
@@ -1,5 +1,6 @@
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys 
 import pandas as pd
-driver = webdriver.Chrome('chromedriver-win64\chromedriver.exe')
+
+driver = webdriver.Chrome()
 driver.get('https://hoopshype.com/salaries/players/')
