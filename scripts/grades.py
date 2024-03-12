cfdf1547ce7415a9ea2dd8e289cbd34961404610
Eric Liang <eliang17@berkeley.edu>
2024-03-11 19:07:19 -0700

    added grade Distrubtion function for API response

--- scripts/grades.py
+++ scripts/grades.py
@@ -23,20 +23,35 @@ def retrieve_request():
 def get_grade_distribution(request):
     course_gpa = request.get("course_gpa")
     average_letter_grade = request.get("course_letter")
-    course_name = request.get("title")
+    course_name = "CS61A"
 
     a_plus = request.get("A+").get("percent")
     a = request.get("A").get("percent")
-    a_minus = request.get("B").get("percent")
+    a_minus = request.get("A-").get("percent")
     b_plus = request.get("B+").get("percent")
     b = request.get("B").get("percent")
     b_minus = request.get("B-").get("percent")
     c_plus = request.get("C+").get("percent")
     c = request.get("C").get("percent")
-    c_mius = request.get("C-").get("percent")
+    c_minus = request.get("C-").get("percent")
     d = request.get("D").get("percent")
     f = request.get("F").get("percent")
     p = request.get("P").get("percent")
     np = request.get("NP").get("percent")
 
-    
+    dict_grades = {}
+    dict_grades["A+"] = a_plus
+    dict_grades["A"] = a
+    dict_grades["A-"] = a_minus
+    dict_grades["B+"] = b_plus
+    dict_grades["B"] = b
+    dict_grades["B-"] = b_minus
+    dict_grades["C+"] = c_plus
+    dict_grades["C"] = c
+    dict_grades["C-"] = c_minus
+    dict_grades["D"] = d
+    dict_grades["f"] = f
+    dict_grades["P"] = p
+    dict_grades["NP"] = np
+
+    return dict_grades
